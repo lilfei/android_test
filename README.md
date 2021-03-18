faf72a2982a72fb6df6e5719e33831f724bbd33c
Satvr <satvr@126.com>
2021-03-18 12:18:44 +0800

    clear

--- README.md
+++
@@ -1,6 +0,0 @@
-# android_test
-
-### test okhttp
->可使用sshenv调试部分功能，需要自己改ip
-
-### test OpenGL
--- app/src/main/java/com/zzx/okhttptest/mvc/controller/OnResStringListener.java
+++
@@ -1,9 +0,0 @@
-package com.zzx.okhttptest.mvc.controller;
-
-import com.zzx.okhttptest.mvc.model.ResString;
-
-public interface OnResStringListener {
-    void onSuccess(ResString result);
-
-    void onFailure();
-}
